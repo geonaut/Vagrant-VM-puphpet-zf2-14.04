0fa53acb3ccad3326ba2b4d0fd049c4aefb80b94
geonaut <oliverbazely@gmail.com>
2014-06-25 10:40:09 +0100

    Ubuntu / Vagrant / Puppet VM with working ZF2 site
    Installed & Configured via puphpet.com:
    
    VBox as Hypervisor
    Ubuntu 12.04 Base box
    Puppet
    Apache2
    MariaDb
    PHP 5.5 with xDebug
    2 vhosts
    
    My added configuration
    
    RVM Multi-user (enabled as function)
    Ruby 2.1.2 (default)
    Node.js
    n
    npm
    Bower
    Grunt
    Foundation 5
    Compass
    Bazaar
    Zend Framework 2 Skeleton App
    Foundation 5 Template

---
+++ setup-sites.sh
@@ -0,0 +1,13 @@
+echo 'Downloading Zend Skeleton App'
+cd /.
+cd vagrant
+rm -fr zend-app.local
+curl -sS https://getcomposer.org/installer | php
+sudo php composer.phar create-project --repository-url="http://packages.zendframework.com" zendframework/skeleton-application:dev-master /vagrant/zend-app.local/skeleton-application
+
+echo 'Setting Up Foundation / Grunt Test Site'
+rm -fr foundation-test.local
+foundation new foundation-test.local --libsass
+cd foundation-test.local
+npm install
+grunt build
