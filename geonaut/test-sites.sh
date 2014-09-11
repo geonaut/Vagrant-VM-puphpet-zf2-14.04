bba81b6385a7ebc1c61c8d6c5230b36cd757f023
geonaut <oliverbazely@gmail.com>
2014-09-11 16:28:49 +0100

    Commented out deletion of test_site folder
    Only use when repeatedly using test-sites.sh

--- geonaut/test-sites.sh
+++ geonaut/test-sites.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+cd /vagrant
+mkdir Test_Sites
+
 echo 'Downloading Zend Skeleton App'
 cd /vagrant/Test_Sites
-rm -fr zend-app.local
+#echo 'removing any previous Zend test site'
+#rm -fr zend-app.local
 curl -sS https://getcomposer.org/installer | php
 sudo php composer.phar create-project --repository-url="http://packages.zendframework.com" zendframework/skeleton-application:dev-master /vagrant/Test_Sites/zend-app.local/skeleton-application
 
 echo 'Setting Up Foundation / Grunt Test Site'
 cd /vagrant/Test_Sites
-rm -fr foundation-test.local
+#echo 'removing any previous foundation test site'
+#rm -fr foundation-test.local
 foundation new foundation-test.local --libsass
 cd foundation-test.local
 npm install grunt-sass grunt-contrib-watch
